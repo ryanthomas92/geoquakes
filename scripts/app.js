d6ff454a7f8d5cd491498563f68ec0de41bbf1a3
Ryan_Thomas <rthoma11@villanova.edu>
2016-12-07 15:01:26 -0800

    steps one and two

--- scripts/app.js
+++ scripts/app.js
@@ -4,5 +4,34 @@ var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/s
 $(document).on("ready", function() {
 
   // CODE IN HERE!
+$.ajax({
+  method: "get",
+  url: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson",
+  dataType: "json",
+  data: $("body").serialize(),
+  success: success,
+})
 
+
+
+function success(json) {
+  json.features.forEach(function(quakes){
+    var timeSince = time(quakes);
+    var templateHtml = template({
+        magnitude: quakes.properties.mag,
+        place: quakes.properties.place,
+        time: timeSince
+      });
+      $("#info").append(templateHtml);
+  });
+}
+
+var source = $("#info-template").html();
+var template = Handlebars.compile(source);
 });
+
+function time(quakes) {
+  var quakeTime = quakes.properties.time;
+  var now = Date.now();
+  return ((now - quakeTime)/(1000 * 60 * 60 * 24)).toFixed(2);
+}
